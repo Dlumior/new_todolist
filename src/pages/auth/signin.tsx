629af1980542d945e9c462fabeb373405292e547
Dlumior <ld_ramirez27@outlook.com>
2023-08-10 16:55:02 -0500

    fix: some text changes

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -24,7 +24,7 @@ const Signin = ({ providers }: { providers: InferGetServerSidePropsType<typeof g
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-slate-800">
       <div className="mb-10 ">
-        <h2 className="text-center text-2xl font-bold text-white">Bezzy</h2>
+        <h2 className="text-center text-2xl font-bold text-white">Bezzy!</h2>
         <span className="text-center text-sm font-light text-white"> Todo list App</span>
       </div>
       <div>
